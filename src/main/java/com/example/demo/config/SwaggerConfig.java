0b2b718349d59fd0f74ebce185be608880e038af
bala-amypo <bala@amypo.in>
2025-12-20 08:10:01 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class SwaggerConfig {
+    
 }
